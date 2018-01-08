23ed9fd6aa45fa123d1c7bd7c36033a587433837
Suz Hinton <suz.hinton@gmail.com>
2018-01-07 22:08:05 -0500

    hopefully fixes #14 :pray:

--- test/helpers/messages.js
+++ test/helpers/messages.js
@@ -3,25 +3,38 @@ module.exports = {
     Content-Type: application/json; charset=utf-8\r
     X-RequestId: 123e4567e89b12d3a456426655440000\r
 \r
-{ "RecognitionStatus": "Success", "DisplayText": "Remind me to buy 5 pencils.", "Offset": 0, "Duration": 12300000 }`,
+{\r
+  "RecognitionStatus": "Success",\r
+  "DisplayText": "Remind me to buy 5 pencils.",\r
+  "Offset": 0,\r
+  "Duration": 12300000\r
+}`,
 
   'speech.startDetected': `Path: speech.startDetected\r
     Content-Type: application/json; charset=utf-8\r
     X-RequestId: 123e4567e89b12d3a456426655440000\r
 \r
-    { "Offset": 100000 }`,
+    {\r
+      "Offset": 100000\r
+    }`,
 
     'speech.endDetected': `Path: speech.endDetected\r
       Content-Type: application/json; charset=utf-8\r
       X-RequestId: 123e4567e89b12d3a456426655440000\r
 \r
-      { "Offset": 0 }`,
+      {\r
+        "Offset": 0\r
+      }`,
 
     'turn.start': `Path: turn.start\r
       Content-Type: application/json; charset=utf-8\r
       X-RequestId: 123e4567e89b12d3a456426655440000\r
 \r
-      {"context": { "serviceTag": "7B33613B91714B32817815DC89633AFA" } }`,
+      {\r
+        "context": {\r
+          "serviceTag": "7B33613B91714B32817815DC89633AFA"\r
+        }\r 
+      }`,
 
     'turn.end': `Path: turn.end\r
       X-RequestId: 123e4567e89b12d3a456426655440000`
